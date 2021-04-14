7e1a0c7d964847942ca0f52c00a1d35c0397c827
nicollesenam <82225853+nicollesenam@users.noreply.github.com>
2021-04-14 10:30:43 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+class Main {
+	public static void main(String[] args) {
+		Apolice novoObj = new Apolice();
+
+
+		Scanner leituraConsole = new Scanner(System.in);
+
+		System.out.println("Informe o nome do segurado:");
+
+		novoObj.setNomeSegurado(leituraConsole.nextLine());
+		
+		System.out.println("Informe a idade:");
+	
+		novoObj.setIdade(Integer.valueOf(Integer.valueOf(leituraConsole.nextLine())));
+
+		System.out.println("Informe o valor do prêmio:");
+		novoObj.setValorPremio(Double.valueOf(leituraConsole.nextLine()));
+
+		novoObj.imprimir();
+		System.out.println("-------");
+
+		novoObj.calcularPremioApolice();
+		novoObj.imprimir();
+		System.out.println("-------");
+
+		novoObj.oferecerDesconto("belo horizonte");
+		novoObj.imprimir();
+		System.out.println("-------");
+	}
+}
